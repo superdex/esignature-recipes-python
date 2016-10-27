0816a7a65decd252ae7576d89b02a586b1f11248
larry-kluger <larry.kluger@docusign.com>
2016-10-27 12:19:17 +0300

    Support new trace information in API Log entries

--- app/static/assets_master/api_logs.js
+++ app/static/assets_master/api_logs.js
@@ -312,10 +312,25 @@
             
             parsed.request.url = line1.split(" ")[1];
             raw = raw.substring(end_of_line1 + eol_size); // remove first line
-            var end_of_headers = raw.indexOf(eol + eol);
-            parsed.request.headers = raw.substring(0, end_of_headers).replace("\r\n", "\n");
-            raw = raw.substring(end_of_headers + eol_size * 2);  // Now raw starts at the beginning of the request body
+            var end_of_trace = raw.indexOf(eol + eol);
+            parsed.request.trace = raw.substring(0, end_of_trace).replace("\r\n", "\n");
+            raw = raw.substring(end_of_trace + eol_size * 2);  // Now raw starts at the beginning of the headers body
 
+            // NOTE: the request may or may not include the trace information. 
+            // If it doesn't, then the "trace" info is really the header info.
+            
+            if (parsed.request.trace.substring(0, 15) == "Content-Length:" ||
+                parsed.request.trace.substring(0, 11) == "Connection:") {
+                // No trace info
+                parsed.request.headers = parsed.request.trace;
+                parsed.request.trace = false;
+            } else {
+                // Trace info provided. Pull out the headers
+                var end_of_headers = raw.indexOf(eol + eol);
+                parsed.request.headers = raw.substring(0, end_of_headers).replace("\r\n", "\n");
+                raw = raw.substring(end_of_headers + eol_size * 2);  // Now raw starts at the beginning of the request body
+            }
+            
             // Find the request Content-Type. Eg Content-Type: application/pdf
             // NB, the request may not have a content type!
             var ct = content_type(parsed.request.headers);
