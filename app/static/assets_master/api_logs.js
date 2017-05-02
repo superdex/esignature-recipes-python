0d66b42a62adce169f61124c315a792d87365ec9
larry-kluger <larry.kluger@docusign.com>
2017-05-03 02:21:46 +0300

    Update Ace 1.2.6 and better way to set mode
    See
    http://stackoverflow.com/questions/42645938/ace-modes-for-xml-json-work-
    but-not-liquid/42652042#42652042

--- app/static/assets_master/api_logs.js
+++ app/static/assets_master/api_logs.js
@@ -1,18 +1,18 @@
 // Javascript for API logging
 
 ;(function ($) {
-  
+
 	// Private global variabls for the functions
 	var toc_items = [], // array of the notification items that are being displayed
 		ace_editors = {request: false, response: false},
-        ace_cdn = "https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.3/", // Where ACE loads optional JS from
+        ace_cdn = "https://cdn.jsdelivr.net/ace/1.2.6/noconflict/"
         omit_base64_text = "[Base64 data omitted]",
         omit_image_text  = "[Image data omitted]",
         item_intro_template,
         request_tab_template,
         raw_tab_template,
         response_tab_template;
-            
+
     // FORMAT OF THE TOC ENTRIES
     // toc_entries is an array. The element [0] is the oldest, shown at the bottom of the page.
     // Each entry is {
@@ -26,7 +26,7 @@
     //    body: the NOT uuencoded first 1500 bytes of the log entry
     //    date_time: the call was made before this time
     // }
-	
+
 	//////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////
 
@@ -42,7 +42,7 @@
         var feedback = $('#logging-download').attr("data-feedback");
         $('#logging-download').click(feedback, logs_download);
     }
-    
+
     var logging_status = function _logging_status(e) {
         var logging_status = $("#logging-status"),
             counter_el = $(logging_status).next()[0],
@@ -62,7 +62,7 @@
         })
             .done(function (data, textStatus, jqXHR) {
                 if (data.err && data.hasOwnProperty("err_code") && data.err_code === "PLEASE_AUTHENTICATE") {
-                    $(logging_status).html("<b>Problem:</b> " + data.err + 
+                    $(logging_status).html("<b>Problem:</b> " + data.err +
                     " <a class='btn btn-primary' role='button' href='..'>Authenticate</a>");
                 } else if (data.err && data.hasOwnProperty("err_code") && data.err_code === "PLEASE_REAUTHENTICATE") {
                     $(logging_status).html("<b>Problem:</b> Authentication has expired. Re-authentication in 3 seconds");
@@ -81,7 +81,7 @@
                 $(counter_el).text("")
             });
     }
-    
+
     var logs_download = function _logs_download(e){
         var feedback = e.data,
             feedback_el = $("#" + feedback),
@@ -121,9 +121,9 @@
             .always(function(){window.setTimeout(logging_status, 0)
             })
     }
-    
+
     function logs_initial_download(){
-        // Do the initial download of log entries that were already on the server 
+        // Do the initial download of log entries that were already on the server
         // (not DocuSign platform)
         var feedback = "working",
             feedback_el = $("#" + feedback),
@@ -159,12 +159,12 @@
         //   url: url for retrieving the complete file
 		// }
         //
-        // 
+        //
         data = data.entries;
 		if (data.length == 0) {
             stop_feedback(); // nothing to do...
             return;
-        } 
+        }
 
 		// Sort the incoming events so the newest (_00) is first.
 		data.sort(function (a, b) {
@@ -175,7 +175,7 @@
 		data.forEach (function(val, index, arr){add_to_toc(examine(val), animate)});
         stop_feedback();
     }
-	
+
     function examine(item){
         // Compute the item's information
         //    success: boolean -- was the call successful?
@@ -205,7 +205,7 @@
             re_time_method_r = re_time_method.exec(item.file_name),
             method_name = re_time_method_r ? re_time_method_r[2] : false,
             date_time = re_time_method_r ? re_time_method_r[2].replace("_", ":").replace("T", " ") : false;
-        
+
         item.success = success;
         item.x_ray = x_ray;
         item.method = method;
@@ -218,7 +218,7 @@
 	function add_to_toc(item, animate){
         // Set css class depending on success field
         item.css_class = item.success ? "success-circle" : "failure-circle";
-        
+
 		toc_items.push(item); // We're assuming that we won't receive an item out of order.
 		// Create the new li by using mustache with the template
 	    var rendered = Mustache.render($('#toc_item_template').html(), item);
@@ -226,7 +226,7 @@
 		// The new item is now the first item in the toc ul.
 		$("#toc").children().first().click(item, show_item).tooltip(); // See http://getbootstrap.com/javascript/#tooltips
 	}
-	
+
 	function prependListItem(listName, listItemHTML, animate){
 		// See http://stackoverflow.com/a/1851486/64904
 	    if (animate) {
@@ -247,7 +247,7 @@
 		// It fills in the main column
 		var item = event.data, // our object about the log entry
 			item_info_el = $("#item-feedback");
-	
+
 		$(item_info_el).html("<h2>Working...</h2>").show();
 		$.ajax({url: item.url, type: 'get'})
 		    .fail(function(jqXHR, textStatus, errorThrown) {
@@ -258,7 +258,7 @@
 	}
 
     var do_show_item = function(data, textStatus, jqXHR, item) {
-        
+
         var parsed, omit_base64;
         // Parse the item into parsed: {
         //   raw
@@ -288,7 +288,7 @@
         //      body
         //      show_editor
         //      json // the response, parsed into a json object (iff json_ok)
-        
+
         function parse_it(){
             var raw = data,
                 re_cr_style = /\r\n\r\n/m,
@@ -297,7 +297,7 @@
                 eol_size = eol.length,
                 end_of_line1 = raw.indexOf(eol),
                 line1 = raw.substring(0, end_of_line1).replace("\r\n", "\n");
-                    
+
             parsed = {
                 raw: data,
                 request:{
@@ -309,30 +309,30 @@
                     success: item.success,
                     success_class: item.success ? "success-circle" : "failure-circle"
             }};
-            
+
             // First, treat the incoming data as one long string and change any
-            // "documentBase64": "JVB blah blah" to 
-            // "documentBase64": "[Base64 data omitted]" 
+            // "documentBase64": "JVB blah blah" to
+            // "documentBase64": "[Base64 data omitted]"
             if (omit_base64) {
                 var re_omit64 = /"documentBase64":\s*"([^"]*)"/g;
                 raw = raw.replace(re_omit64, '"documentBase64":"' + omit_base64_text +'"');
-                
+
                 var re_omit_pdf_bytes = /\<PDFBytes\>([^\<]*)\</g;
                 raw = raw.replace(re_omit_pdf_bytes, '<PDFBytes>' + omit_base64_text +'<');
-                                
+
                 var re_omit_images = /"imageBytes":\s*"([^"]*)"/g;
                 raw = raw.replace(re_omit_images, '"imageBytes":"' + omit_image_text +'"');
             }
-            
+
             parsed.request.url = line1.split(" ")[1];
             raw = raw.substring(end_of_line1 + eol_size); // remove first line
             var end_of_trace = raw.indexOf(eol + eol);
             parsed.request.trace = raw.substring(0, end_of_trace).replace("\r\n", "\n");
             raw = raw.substring(end_of_trace + eol_size * 2);  // Now raw starts at the beginning of the headers body
 
-            // NOTE: the request may or may not include the trace information. 
+            // NOTE: the request may or may not include the trace information.
             // If it doesn't, then the "trace" info is really the header info.
-            
+
             if (parsed.request.trace.includes("Timestamp")) {
                 // Trace info provided. Pull out the headers
                 var end_of_headers = raw.indexOf(eol + eol);
@@ -343,7 +343,7 @@
                 parsed.request.headers = parsed.request.trace;
                 parsed.request.trace = false;
             }
-            
+
             // Find the request Content-Type. Eg Content-Type: application/pdf
             // NB, the request may not have a content type!
             var ct = content_type(parsed.request.headers);
@@ -351,7 +351,7 @@
             parsed.request.content_type_json = ct && ct === "application/json";
             parsed.request.content_type_multipart = ct && ct.includes("multipart");
             parsed.request.content_type_xml = ct && ct.includes("text/xml");
-            
+
             var re_status = /^\d{3} [A-Z][A-Za-z]{1,}$/m,
                 end_of_req_body_index = raw.search(re_status);
 
@@ -363,14 +363,14 @@
             var cr_index = raw.indexOf(eol);
             parsed.response.status = raw.substring(0, cr_index);
             raw = raw.substring(cr_index + eol_size); // Now raw starts with the response headers
-            
+
             if (raw == "[SOAP Response body omitted]") {
                 // We're in an ommitted SOAP response
                 parsed.response.headers = "";
             } else {
                 end_of_headers = raw.indexOf(eol + eol);
                 parsed.response.headers = raw.substring(0, end_of_headers).replace("\r\n", "\n");
-                raw = raw.substring(end_of_headers + eol_size * 2);  // Now raw starts at the beginning of the response body                
+                raw = raw.substring(end_of_headers + eol_size * 2);  // Now raw starts at the beginning of the response body
             }
 
             // Find the response content type. Eg Content-Type: application/pdf
@@ -386,10 +386,10 @@
             if (parsed.response.body === "" || parsed.response.body === eol) {parsed.response.body = false}
             parsed.response.show_editor = parsed.response.body !== false && !parsed.response.content_type_multipart;
             parsed.response.content_download_show = "";
-            
+
             // The following needs API-5319 to be fixed before it can be implemented
             //if (parsed.response.content_type == "application/pdf") {
-            //    parsed.response.content_download = "<a id='req_body_download' download='response_pdf.pdf'" + 
+            //    parsed.response.content_download = "<a id='req_body_download' download='response_pdf.pdf'" +
             //    " type='application/octet-stream'>Download the response pdf</a>";
             //}
 
@@ -416,15 +416,15 @@
         $("#request-tab").html(request_tab_template(parsed));
         $("#response-tab").html(response_tab_template(parsed));
         $("#raw-tab").html(raw_tab_template(parsed));
-        $("#raw_download").html("<a download='" + parsed.request.method_name + 
+        $("#raw_download").html("<a download='" + parsed.request.method_name +
         ".txt' type='application/octet-stream'>Download the raw log entry</a>");
-        
+
         var d = new Blob([parsed.raw], {type : 'octet/stream'});
         $("#raw_download a").attr("href", URL.createObjectURL(d));
-        
+
         // The following needs API-5319 to be fixed before it can be implemented
         //if (parsed.response.content_type == "application/pdf") {
-        //    
+        //
         //    // This conversion may not be necessary. Test after API-5319 is fixed...
         //    var i, l, d, array;
         //    d = parsed.response.body;
@@ -434,32 +434,30 @@
         //        array[i] = d.charCodeAt(i);
         //    }
         //    var b = new Blob([array], {type: 'application/octet-stream'});
-        //                
+        //
         //    // d = new Blob([parsed.response.body], {type : 'octet/stream'});
         //    $("#req_body_download").attr("href", URL.createObjectURL(b));
         //}
-        
+
         // Add content to the editor windows as appropriate
-        // Editors will be in 
-        //   <div id="request-body-editor" class="log_editor"></div>                                
-        //   <div id="response-body-editor" class="log_editor"></div>                                
+        // Editors will be in
+        //   <div id="request-body-editor" class="log_editor"></div>
+        //   <div id="response-body-editor" class="log_editor"></div>
         create_editors();
-        var XMLMode = ace.require("ace/mode/xml").Mode,
-            JSONMode = ace.require("ace/mode/json").Mode;
-        
+
         ["request", "response"].forEach(function(i){
-            if (parsed[i].show_editor) {            
-                $("#" + i + "-body-editor").show();   
+            if (parsed[i].show_editor) {
+                $("#" + i + "-body-editor").show();
                 var value = parsed[i].body,
                     ace_session = ace_editors[i].getSession();
-                
+
                 if (parsed[i].content_type_json && parsed[i].json_problem === false) {
                     value = JSON.stringify(parsed[i].json, null, 4);
-                    ace_session.setMode(new JSONMode());
+                    ace_session.setMode("/ace/mode/json");
                 }
                 if (parsed[i].content_type_xml) {
                     value = vkbeautify.xml(parsed[i].body, 4);
-                    ace_session.setMode(new XMLMode());
+                    ace_session.setMode("/ace/mode/xml");
                 }
                 ace_editors[i].setValue(value);
                 ace_editors[i].getSelection().clearSelection();
@@ -474,6 +472,7 @@
     function create_editors(){
         if (ace_editors.request) {return}
         ace.config.set("workerPath", ace_cdn);
+        ace.config.set("basePath", ace_cdn);
 
         ["request", "response"].forEach(function(i){
             ace_editors[i] = ace.edit(i + "-body-editor");
@@ -482,13 +481,8 @@
             ace_editors[i].setOption("wrap", "free");
             ace_editors[i].$blockScrolling = Infinity;
             window_resized();
-            var XMLMode = ace.require("ace/mode/xml").Mode,
-                JSONMode = ace.require("ace/mode/json").Mode,
             ace_session = ace_editors[i].getSession();
-            // ace_session.setMode(new XMLMode());
-            ace_session.setMode(new JSONMode());
-
-            // ace_session.setMode("ace/mode/json");
+            ace_session.setMode("ace/mode/json");
             ace_session.setUseWrapMode(true);
             ace_session.setFoldStyle("markbeginend");
         })
@@ -510,26 +504,26 @@
 	var window_resized = function(){
 		// resize left column
 		var available = window.scrollHeight ? window.scrollHeight : $(window).height(),
-			h = available -  $("#status_left").position().top; 
+			h = available -  $("#status_left").position().top;
 			// At least for Chrome, scrollHeight is only defined if there is a scrollbar
-		
+
 		// if ($(window).width() > 991) { $("#status_left").height(h); }
-		
+
 		// resize editor div, 300 min
 		var min = 300;
-		// if (ace_editor) {			
+		// if (ace_editor) {
 		// 	h = $(window).height() - $("#editor").offset().top - $(".navbar").height() - 5;
 		// 	// The navbar is fixed so offset doesn't include it. (?)
-        // 
+        //
 		// 	$("#editor").height((h < min) ? min : h);
 		// 	ace_editor.resize();
 		// }
 	}
-    
+
     function set_tab_visible_handlers(){
         // Whenever a tab that has an editor in it becomes visible, we
         // need to call resize on the editor or its current data is not shown(!)
-        
+
         $('a[data-refresh]').on('shown.bs.tab', function (e) {
             // e.target // newly activated tab
             // data-refresh=request
@@ -543,7 +537,7 @@
 		countdown_i -= 1;
 		$("#counter").text(countdown_i);
 	}
-	
+
 	function stop_countdown(){
 		if (! countdown_interval_id) {return;}
 		clearInterval(countdown_interval_id);
@@ -551,25 +545,25 @@
 		$("#countdown").hide();
 		$("#xml_info").html("<h1 class='toc-instructions'>← Click an item to see the XML file</h1>");
 	}
-	
+
 	function working_show(){
 		$("#working").removeClass("workinghide").addClass("workingshow");
 	}
-	
+
 	function working_hide(){
 		$("#working").removeClass("workingshow").addClass("workinghide");
 	}
-	
+
 	//////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////
-	
+
 	function xml_make_pretty (xml) {
 		// From https://gist.github.com/sente/1083506
 		var formatted = '',
 		    reg = /(>)(<)(\/*)/g,
 			pad = 0,
 			pad_content = "    ";
-			
+
 		xml = xml.replace(reg, '$1\r\n$2$3');
 
 		jQuery.each(xml.split('\r\n'), function(index, node) {
@@ -597,11 +591,11 @@
 
 		return formatted;
 	}
-	
-	
+
+
 	//////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////
-	  
+
 	// the mainline
 	$(document).ready(function() {
         if ($(".api_logs").length == 0) {
@@ -612,14 +606,14 @@
         request_tab_template = Handlebars.compile($("#request-tab-template").html());
         response_tab_template = Handlebars.compile($("#response-tab-template").html());
         raw_tab_template = Handlebars.compile($("#raw-tab-template").html());
-        
+
         logs_initial_download();
 	    window_resized();
         create_editors();
         set_tab_visible_handlers();
 	    $(window).resize(window_resized);
 	});
-	
+
 }(jQuery));
 
 	//////////////////////////////////////////////////////////////////////////////
@@ -629,5 +623,3 @@
 
 // Polyfill for string.includes. See https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 String.prototype.includes||(String.prototype.includes=function(t,e){"use strict";return"number"!=typeof e&&(e=0),e+t.length>this.length?!1:-1!==this.indexOf(t,e)});
-
-
