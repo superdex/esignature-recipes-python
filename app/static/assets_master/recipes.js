93651427b2ae2e3d7f7aa66c3a77384c183793b4
larry-kluger <larry.kluger@docusign.com>
2017-05-03 10:51:56 -0700

    Fix date time comparison bug

--- app/static/assets_master/recipes.js
+++ app/static/assets_master/recipes.js
@@ -5,21 +5,21 @@
 // JavaScript helper for the 010.webhook recipe
 // This is NOT the Node.js recipe!
 ;(function ($) {
-  
+
   	// page-level parameters are set via ds_params.
 	// Eg {'navbar' : 'li_home'}
     // ds_params is set by the page as a script tag when the page is loaded.
-	// 
+	//
 	// Private global variabls for the functions
 	var toc_items = [], // array of the notification items that are being displayed
 		// The array is in normal sort order -- latest item is at the end
-		envelope_terminal_statuses = ["Completed", "Declined", "Voided", 
+		envelope_terminal_statuses = ["Completed", "Declined", "Voided",
 			"AuthoritativeCopy", "TransferCompleted", "Template"],
 	 	countdown_interval_id = false,
 		countdown_i,
 		ace_editor = false,
         ace_cdn = "https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.3/"; // Where ACE loads optional JS from
-	
+
 	//////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////
     //
@@ -74,14 +74,14 @@
                 $(busy).hide();
 			})
 	}
-	
+
 	function auth_status_display(data){
 		// Save the auth info in the authention information panel
 		var panel = "#authentication-info",
 			auth = data["auth"],
 		  	text = [];
-		
-		
+
+
         // auth: {authenticated: true/false
         //        user_name
         //        email
@@ -91,7 +91,7 @@
         //        auth_header_key    # Used for API calls
         //        auth_header_value  # Used for API calls
         //        refresh_token # OAuth code grant only
-		
+
 		if (! auth.authenticated) {
 			text.push("<p>Not authenticated.</p>");
 		} else {
@@ -116,8 +116,8 @@
 		$(panel).html(text.join("\n"));
 		set_ajax_buttons();
 	}
-	
-    
+
+
 	function webhook_status(always_show = false){
         // Check the webhook status.
         // If always_show or "ask" then ask the user what should be done.
@@ -138,7 +138,7 @@
                 $(busy).hide();
             })
 	}
-    
+
     function show_webhook_status(status) {
         // Set the form with the status info
         $('#webhook_status option[value=' + status.status + ']').attr('selected','selected');
@@ -147,8 +147,8 @@
         $("#feedback-webhook").html("<h3>Working...&nbsp;&nbsp;&nbsp;<span></span></h3>").hide();
         $("#options-form").show();
     }
-    
-    
+
+
 	//////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////
 
@@ -174,7 +174,7 @@
     }
 
     function set_ajax_buttons() {
-        // An example button: 
+        // An example button:
         // <button id="startbtn" type="button" class="btn btn-primary"
         // data-endpoint="login" data-response="newpage_get">Login!</button>
         //
@@ -189,7 +189,7 @@
         // 2. Add extra html after the button
 		//
 		// ALSO: Sets up listeners for close buttons
-        var feedback_html = 
+        var feedback_html =
             "<div class='feedback' style='display:none;'><h3>Working...&nbsp;&nbsp;&nbsp;<span></span></h3></div>";
 
         $('button[data-response]').each(function each_ajax_button (i, el){
@@ -224,13 +224,13 @@
             countdown = function(){countdown_i -= 1; $(counter_el).text(countdown_i)},
             countdown_id
         ;
-        
+
         countdown_i = 100;
         button_disable(el);
         feedback_el.show();
         counter_el.text(countdown_i);
 		countdown_id = setInterval(countdown, 300);
-        
+
         // newpage responses: simply transfer over to the new page
         if (response==="newpage_get") {
             window.location = endpoint;
@@ -304,27 +304,27 @@
         }
 
 
-        
+
 	//////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////
 
 	function set_nav_bar () {
 		// Uses the navbar element of ds_params
-		if (typeof ds_params !== 'undefined' && ds_params !== null 
+		if (typeof ds_params !== 'undefined' && ds_params !== null
 			&& typeof ds_params === 'object' && ds_params.navbar !== undefined ) {
-			
+
 			$('#' + ds_params.navbar).addClass("active");
 		}
 	}
-    
+
 	function button_disable(id) {
 		$(id).attr("disabled", "disabled");
 	}
-	
+
 	function button_enable(id) {
-		$(id).removeAttr("disabled");			
+		$(id).removeAttr("disabled");
 	}
-	
+
 	//////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////
 	//
@@ -332,23 +332,23 @@
 	// Functions for showing the incoming events
 	// Add envelope info to #env_info
 	// The left column ul is #toc, the main column uses xml_info for info and feedback
-	
+
 	function show_status() {
-		if (typeof ds_params === 'undefined' || ds_params === null 
-			|| typeof ds_params !== 'object' 
+		if (typeof ds_params === 'undefined' || ds_params === null
+			|| typeof ds_params !== 'object'
 			|| typeof ds_params.status_envelope_id === 'undefined'
 			|| !ds_params.status_envelope_id) {
 				return;
 		}
-		
+
 		// We're good to go!
 		var envelope_id = ds_params.status_envelope_id,
 			interval_id;
-		
+
 		// Keep the humans occupied..
         countdown_i = 200
 		countdown_interval_id = setInterval(countdown, 300);
-		
+
 		var fetch_latest = function (){
 			// This function fetches the latest info from the server
 			working_show();
@@ -365,18 +365,18 @@
 			})
 			.always(function() {working_hide();})
 		}
-		
+
 		// The main stem...
 		window_resized();
 		$(window).resize(window_resized);
 		interval_id = setInterval(fetch_latest, 3000);
 	}
-		
+
 	function process_items(data){
 		// Process the list of items
 		// The data is an array of events. Events are objects
 		// [event]
-		// event :: 
+		// event ::
 		// { envelope_id,
 		//	 time_generated,
 		//   subject
@@ -385,8 +385,8 @@
 		//	 envelope_status
 		//	 timezone_offset
 		//   recipients: [recipient]
-		//   documents: [document]	
-		// }	
+		//   documents: [document]
+		// }
 		//
 		// recipient ::
 		// { type
@@ -401,13 +401,13 @@
 		// { document_ID
 		//   document_type
 		//   name
-		//   url 
+		//   url
 		// }
 		// RETURNS stop_fetch -- should we stop querying?
-		
+
 		if (data.length == 0) {return;} // nothing to do...
 		stop_countdown();
-		
+
 		// Sort the incoming events
 		data.sort(function (a, b) {
   			// compare time_generated eg 2016-01-01T01:07:04.1479113
@@ -416,42 +416,42 @@
 				a_datetime = new Date(a_parts[0]),
 				b_datetime = new Date(b_parts[0]);
 			if (a_datetime > b_datetime) {return 1;}
-			if (a_datetime < a_datetime) {return -1;}
+			if (a_datetime < b_datetime) {return -1;}
 			if (a_parts[1] > b_parts[1]) {return 1;}
 			if (a_parts[1] < b_parts[1]) {return -1;}
 			return 0; // a must be equal to b
 	  	});
-	
+
 		// remove incoming items that we aleady know about
 		var data_new  = data.filter (function (val, index, arr){
 			return !toc_items.find(function (element, i, a){
 				return val.time_generated == element.time_generated;
 			});
 		});
-		
+
 		if (data_new.length == 0) {return;} // nothing to do...
-	
+
 		// display the new data
 		data_new.forEach (function(val, index, arr){add_to_toc(val);});
 		var latest = data_new[data_new.length - 1];
-		
+
 		return envelope_terminal_statuses.includes(latest.envelope_status); // envelope done?
 	}
-	
+
 	function add_to_toc(item){
 		// Augment the incoming data by comparing it with the prior data
 		var toc_items_latest = toc_items.length > 0 ? toc_items[toc_items.length - 1] : false,
 			status_class_new_data = "newdata",
 			status_class_same_data = "";
-    	
+
 		// add envelope_status_class
 		if (toc_items_latest) {
-			item.envelope_status_class = (item.envelope_status == toc_items_latest.envelope_status) ? 
+			item.envelope_status_class = (item.envelope_status == toc_items_latest.envelope_status) ?
 				status_class_same_data : status_class_new_data;
 		} else {
 			item.envelope_status_class = status_class_new_data;
 		}
-    	
+
 		// add status_class for each recipient
 		item.recipients.forEach (function(val, index, arr){
 			if (toc_items_latest) {
@@ -461,18 +461,18 @@
 						val.type == element.type && // Eg a person can be both a signer and later receive a specific cc or cd
 						val.routing_order == element.routing_order ; // Eg, a person could sign twice, once after someone else
 				});
-				
+
 				if (previous) {
-					val.status_class = (val.status == previous.status) ? 
+					val.status_class = (val.status == previous.status) ?
 						status_class_same_data : status_class_new_data;
 				} else {
 					val.status_class = status_class_new_data;
-				} 
+				}
 			} else {
 				val.status_class = status_class_new_data;
 			}
 		});
-	
+
 		toc_items.push(item); // We're assuming that we won't receive an item out of order.
 		// Create the new li by using mustache with the template
 	    var rendered = Mustache.render($('#toc_item_template').html(), item);
@@ -480,7 +480,7 @@
 		// The new item is now the first item in the toc ul.
 		$("#toc").children().first().click(item, show_xml);
 	}
-	
+
 	// prependListItem("test", "<li>The new item</li>");
 	function prependListItem(listName, listItemHTML){
 		// See http://stackoverflow.com/a/1851486/64904
@@ -498,7 +498,7 @@
 		// It fills in the main column
 		var item = event.data, // our object about the xml notification
 			item_info_el = $("#xml_info");
-	
+
 		$(item_info_el).html("<h2>Working...</h2>");
 		$.ajax({
 			url: item.xml_url,
@@ -507,7 +507,7 @@
 		.fail(function(jqXHR, textStatus, errorThrown) {
 		    $(item_info_el).html("<h3>Problem: Couldn’t fetch the xml file</h3><p>" + textStatus + "</p>");
 		})
-		.done(function(data, textStatus, jqXHR) {	
+		.done(function(data, textStatus, jqXHR) {
 			var xml_pretty = xml_make_pretty(jqXHR.responseText),
 				rendered = Mustache.render($('#xml_file_template').html(), item);
 			$(item_info_el).html(rendered);
@@ -534,14 +534,14 @@
 	var window_resized = function(){
 		// resize left column
 		var available = window.scrollHeight ? window.scrollHeight : $(window).height(),
-			h = available -  $("#status_left").position().top; 
+			h = available -  $("#status_left").position().top;
 			// At least for Chrome, scrollHeight is only defined if there is a scrollbar
-		
+
 		if ($(window).width() > 991) { $("#status_left").height(h); }
-		
+
 		// resize editor div, 300 min
 		var min = 300;
-		if (ace_editor) {			
+		if (ace_editor) {
 			h = $(window).height() - $("#editor").offset().top - $(".navbar").height() - 5;
 			// The navbar is fixed so offset doesn't include it. (?)
 
@@ -555,7 +555,7 @@
 		countdown_i -= 1;
 		$("#counter").text(countdown_i);
 	}
-	
+
 	function stop_countdown(){
 		if (! countdown_interval_id) {return;}
 		clearInterval(countdown_interval_id);
@@ -563,25 +563,25 @@
 		$("#countdown").hide();
 		$("#xml_info").html("<h1 class='toc-instructions'>← Click an item to see the XML file</h1>");
 	}
-	
+
 	function working_show(){
 		$("#working").removeClass("workinghide").addClass("workingshow");
 	}
-	
+
 	function working_hide(){
 		$("#working").removeClass("workingshow").addClass("workinghide");
 	}
-	
+
 	//////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////
-	
+
 	function xml_make_pretty (xml) {
 		// From https://gist.github.com/sente/1083506
 		var formatted = '',
 		    reg = /(>)(<)(\/*)/g,
 			pad = 0,
 			pad_content = "    ";
-			
+
 		xml = xml.replace(reg, '$1\r\n$2$3');
 
 		jQuery.each(xml.split('\r\n'), function(index, node) {
@@ -609,11 +609,11 @@
 
 		return formatted;
 	}
-	
-	
+
+
 	//////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////
-	  
+
 	// the mainline
 	$(document).ready(function() {
 		set_nav_bar();
@@ -625,9 +625,9 @@
         // page specific JS
         framework_home_page();
 	});
-	
-	
-	
+
+
+
 }(jQuery));
 
 	//////////////////////////////////////////////////////////////////////////////
@@ -651,6 +651,3 @@ Array.prototype.includes||(Array.prototype.includes=function(r){"use strict";var
  * @version 2.5.0
  */
 !function(e,i){if("function"==typeof define&&define.amd)define(["exports","jquery"],function(e,r){return i(e,r)});else if("undefined"!=typeof exports){var r=require("jquery");i(exports,r)}else i(e,e.jQuery||e.Zepto||e.ender||e.$)}(this,function(e,i){function r(e,r){function n(e,i,r){return e[i]=r,e}function a(e,i){for(var r,a=e.match(t.key);void 0!==(r=a.pop());)if(t.push.test(r)){var u=s(e.replace(/\[\]$/,""));i=n([],u,i)}else t.fixed.test(r)?i=n([],r,i):t.named.test(r)&&(i=n({},r,i));return i}function s(e){return void 0===h[e]&&(h[e]=0),h[e]++}function u(e){switch(i('[name="'+e.name+'"]',r).attr("type")){case"checkbox":return"on"===e.value?!0:e.value;default:return e.value}}function f(i){if(!t.validate.test(i.name))return this;var r=a(i.name,u(i));return l=e.extend(!0,l,r),this}function d(i){if(!e.isArray(i))throw new Error("formSerializer.addPairs expects an Array");for(var r=0,t=i.length;t>r;r++)this.addPair(i[r]);return this}function o(){return l}function c(){return JSON.stringify(o())}var l={},h={};this.addPair=f,this.addPairs=d,this.serialize=o,this.serializeJSON=c}var t={validate:/^[a-z_][a-z0-9_]*(?:\[(?:\d*|[a-z0-9_]+)\])*$/i,key:/[a-z0-9_]+|(?=\[\])/gi,push:/^$/,fixed:/^\d+$/,named:/^[a-z0-9_]+$/i};return r.patterns=t,r.serializeObject=function(){return new r(i,this).addPairs(this.serializeArray()).serialize()},r.serializeJSON=function(){return new r(i,this).addPairs(this.serializeArray()).serializeJSON()},"undefined"!=typeof i.fn&&(i.fn.serializeObject=r.serializeObject,i.fn.serializeJSON=r.serializeJSON),e.FormSerializer=r,r});
-
-
-
